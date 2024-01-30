eafc4c459f4b49d6dfde4989ad4ee90c5712582e
오성애 <ohspdlqj66@naver.com>
2024-01-31 05:44:32 +0900

    #29 - 해시태그 검색 페이지를 위한 기능 구현
    해시태그 뷰에 모든 게시글에서 나오는 해시태그를 정리해서 보여주는 기능을 넣기 위해, 해시태그만 뽑아서 unique하게 보여주는 쿼리를 querydsl 로 작성함
    그 외 서비스 코드, 컨트롤러, 관련 테스트 작성

--- src/main/java/org/project/projectboard/repository/ArticleRepository.java
+++ src/main/java/org/project/projectboard/repository/ArticleRepository.java
@@ -4,6 +4,7 @@ import com.querydsl.core.types.dsl.DateTimeExpression;
 import com.querydsl.core.types.dsl.StringExpression;
 import org.project.projectboard.domain.Article;
 import org.project.projectboard.domain.QArticle;
+import org.project.projectboard.repository.querydsl.ArticleRepositoryCustom;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -15,6 +16,7 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 @RepositoryRestResource
 public interface ArticleRepository extends
         JpaRepository<Article, Long>,
+        ArticleRepositoryCustom,
         QuerydslPredicateExecutor<Article>,
         QuerydslBinderCustomizer<QArticle>
 {
---
+++ src/main/java/org/project/projectboard/repository/querydsl/ArticleRepositoryCustomImpl.java
@@ -0,0 +1,27 @@
+package org.project.projectboard.repository.querydsl;
+
+import com.querydsl.jpa.JPQLQuery;
+import org.project.projectboard.domain.Article;
+import org.project.projectboard.domain.QArticle;
+import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
+
+import java.util.List;
+
+public class ArticleRepositoryCustomImpl extends QuerydslRepositorySupport implements ArticleRepositoryCustom {
+
+    public ArticleRepositoryCustomImpl() {
+        super(Article.class);
+    }
+
+    @Override
+    public List<String> findAllDistinctHashtags() {
+
+        QArticle article = QArticle.article;
+
+        return from(article)
+                .distinct()
+                .select(article.hashtag)
+                .where(article.hashtag.isNotNull())
+                .fetch();
+    }
+}
