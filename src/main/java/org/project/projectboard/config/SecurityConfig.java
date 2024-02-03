6eb77bbcc01799052603e280e6565e8325ae55d2
오성애 <ohspdlqj66@naver.com>
2024-02-03 20:49:22 +0900

    #36 - 버전 변경에 따른 `SecurityConfig` 수정

--- src/main/java/org/project/projectboard/config/SecurityConfig.java
+++ src/main/java/org/project/projectboard/config/SecurityConfig.java
@@ -8,7 +8,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
@@ -24,7 +23,7 @@ public class SecurityConfig {
         return http
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
-                        .requestMatchers(
+                        .mvcMatchers(
                                 HttpMethod.GET,
                                 "/",
                                 "/articles",
@@ -32,13 +31,11 @@ public class SecurityConfig {
                         ).permitAll()
                         .anyRequest().authenticated()
                 )
-                .formLogin((formLogin) ->
-                        formLogin.loginPage("/login"))
-                .logout((logoutConfig) ->
-                        logoutConfig.logoutSuccessUrl("/")
-                )
+                .formLogin().and()
+                .logout()
+                .logoutSuccessUrl("/")
+                .and()
                 .build();
-
     }
 
     @Bean
